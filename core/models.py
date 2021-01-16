bc40c21db00db5bf6c033d051b77d6ac2f043926
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-01-16 15:22:04 +0700

    Update team member and consultant table

--- core/models.py
+++ core/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 
 class TeamMember(models.Model):
-    full_name = models.CharField(max_length=100)
-    position = models.CharField(max_length=100)
+    full_name = models.CharField(max_length=254)
+    position = models.CharField(max_length=254)
 
 
 class Consultant(models.Model):
-    full_name = models.CharField(max_length=100)
-    role = models.CharField(max_length=100)
-    company = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    linkedin_profile = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=100)
+    full_name = models.CharField(max_length=254)
+    role = models.CharField(max_length=254)
+    company = models.CharField(max_length=254)
+    email = models.EmailField(max_length=254)
+    linkedin_profile = models.CharField(max_length=254)
+    phone_number = models.CharField(max_length=254)
     is_active = models.BooleanField()
-    price = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=100, decimal_places=2)
     experience = models.TextField()
-    note = models.TextField()
+    note = models.TextField(null=True, blank=True)
