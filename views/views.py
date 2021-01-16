74e538ac3d1d5d4934e9e80a69fdbde626729e2f
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-01-16 20:07:06 +0700

    Order team members by id

--- views/views.py
+++ views/views.py
@@ -14,7 +14,7 @@ def home(request):
 
 
 def about(request):
-    all_team_members = TeamMember.objects.order_by('order').all()
+    all_team_members = TeamMember.objects.all()
 
     data = {
         "team_members": all_team_members
