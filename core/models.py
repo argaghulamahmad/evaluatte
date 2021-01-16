d76cc0dcb4fe11ece6d1300657cdfcf0c11d931e
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-01-16 16:12:05 +0700

    Create client table

--- core/models.py
+++ core/models.py
@@ -5,6 +5,8 @@ class TeamMember(models.Model):
     profile_image = models.CharField(max_length=254)
     full_name = models.CharField(max_length=254)
     position = models.CharField(max_length=254)
+    linkedin_profile = models.CharField(null=True, blank=True, max_length=254)
+    instagram_profile = models.CharField(null=True, blank=True, max_length=254)
 
 
 class Consultant(models.Model):
@@ -19,3 +21,13 @@ class Consultant(models.Model):
     price = models.DecimalField(null=True, blank=True, max_digits=100, decimal_places=2)
     experience = models.TextField(null=True, blank=True)
     note = models.TextField(null=True, blank=True)
+
+
+class Client(models.Model):
+    full_name = models.CharField(null=True, blank=True, max_length=254)
+    profile_image = models.CharField(null=True, blank=True, max_length=254)
+    cv_url = models.CharField(null=True, blank=True, max_length=254)
+    email = models.EmailField(null=True, blank=True, max_length=254)
+    phone_number = models.CharField(null=True, blank=True, max_length=254)
+    linkedin_profile = models.CharField(null=True, blank=True, max_length=254)
+    instagram_profile = models.CharField(null=True, blank=True, max_length=254)
