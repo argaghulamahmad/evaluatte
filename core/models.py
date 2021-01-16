68c12d6c1daa3ec248bf3288314c54001465b802
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-01-16 15:48:53 +0700

    Update team member and consultant table

--- core/models.py
+++ core/models.py
@@ -7,13 +7,13 @@ class TeamMember(models.Model):
 
 
 class Consultant(models.Model):
-    full_name = models.CharField(max_length=254)
-    role = models.CharField(max_length=254)
-    company = models.CharField(max_length=254)
-    email = models.EmailField(max_length=254)
-    linkedin_profile = models.CharField(max_length=254)
-    phone_number = models.CharField(max_length=254)
+    full_name = models.CharField(null=True, blank=True, max_length=254)
+    role = models.CharField(null=True, blank=True, max_length=254)
+    company = models.CharField(null=True, blank=True, max_length=254)
+    email = models.EmailField(null=True, blank=True, max_length=254)
+    linkedin_profile = models.CharField(null=True, blank=True, max_length=254)
+    phone_number = models.CharField(null=True, blank=True, max_length=254)
     is_active = models.BooleanField()
-    price = models.DecimalField(max_digits=100, decimal_places=2)
-    experience = models.TextField()
+    price = models.DecimalField(null=True, blank=True, max_digits=100, decimal_places=2)
+    experience = models.TextField(null=True, blank=True)
     note = models.TextField(null=True, blank=True)
