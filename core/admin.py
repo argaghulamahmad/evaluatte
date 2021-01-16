69cc03e18250e1c48e08a971e3c3b6a6587cad95
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-01-16 16:29:07 +0700

    Add models to admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import TeamMember, Consultant
+from .models import TeamMember, Consultant, Client, Interview
 
+admin.site.register(Client)
 admin.site.register(Consultant)
+admin.site.register(Interview)
 admin.site.register(TeamMember)
