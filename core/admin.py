d3d286dc8db938dc83a0f73ddeaa810bbe44739e
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-01-18 14:26:56 +0700

    Create ClientAdmin

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,36 @@ from django.contrib import admin
 
 from .models import TeamMember, Consultant, Client, Meet
 
-admin.site.register(Client)
+
+class AdminWithoutModified(admin.ModelAdmin):
+    exclude = ['modified']
+
+    class Meta:
+        abstract = True
+
+
+class ClientAdmin(AdminWithoutModified):
+    list_display = (
+        'full_name',
+        'cv_url',
+        'email',
+        'phone_number',
+        'linkedin_profile',
+        'instagram_profile',
+        'title'
+    )
+    list_display_links = (
+        'full_name',
+    )
+    list_filter = (
+        'title',
+    )
+    ordering = ['id']
+    search_fields = ('id', 'full_name', 'email')
+    list_per_page = 10
+
+
+admin.site.register(Client, ClientAdmin)
 admin.site.register(Consultant)
 admin.site.register(Meet)
 admin.site.register(TeamMember)
