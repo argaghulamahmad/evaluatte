453862495df07040ef399b371e19f923060be16f
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-02-02 21:06:44 +0700

    Create sitemap

---
+++ views/sitemaps.py
@@ -0,0 +1,16 @@
+from django.contrib import sitemaps
+from django.urls import reverse
+
+
+class StaticViewSitemap(sitemaps.Sitemap):
+    priority = 0.5
+    changefreq = 'daily'
+
+    def items(self):
+        return [
+            'home', 'about', 'services', 'service_resume', 'service_interview', 'service_webinar',
+            'testimonials', 'contact'
+        ]
+
+    def location(self, item):
+        return reverse(item)
--- views/urls.py
+++ views/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
-from django.urls import re_path
+from django.contrib.sitemaps.views import sitemap
+from django.urls import re_path, path
 
 from . import views
+from .sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
@@ -17,4 +23,7 @@ urlpatterns = [
             name='consultant-detail-pk-type'),
     url(r'^testimonials/$', views.testimonials, name='testimonials'),
     url(r'^contact/$', views.contact, name='contact'),
+
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap')
 ]
