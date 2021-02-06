0236dc3e42b5d3d8a4440dc1a8181d19c404ad96
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-02-06 18:03:23 +0700

    Remove default acl

--- app/storage_backends.py
+++ app/storage_backends.py
@@ -6,7 +6,6 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class PublicMediaStorage(S3Boto3Storage, ABC):
     location = settings.AWS_PUBLIC_MEDIA_LOCATION
-    default_acl = 'public'
     file_overwrite = False
 
 
