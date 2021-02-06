613c818dd39ab0af6f062640fd522be0a8349e86
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-02-06 10:38:13 +0700

    Update todo.py

---
+++ todo.py
@@ -0,0 +1,50 @@
+# Understanding The Problem
+"""
+- interviewee harus bisa login dan register
+- interviewee harus bisa lihat konsultan nya siapa saja
+    - interviewee harus bisa lihat jadwal free nya si konsultan
+    - interviewee harus bisa booking si konsultan
+    - interviewee harus bisa bayar
+    - interviewee harus bisa menerima email invoice
+    - interviewee harus bisa menerima link google meet dari system
+
+- interviewer dapat mengisi jadwal free mereka
+    - interviewer dapat melihat google meet mereka
+    - interviewer dapat melihat gaji mereka
+
+- chief
+    - dapat melihat berapa meet yang berhasil
+    - dapat melihat berapa total dana yang akan diberikan kepada konsultan dan milik perusahaan
+    - dapat melihat berapa total order yang sudah dibayar dan yang belum
+
+- sales
+    - dapat melihat data konsultan
+    - dapat melihat data client
+    - dapat melihat data order
+
+- ganti logo - done
+- adjust color pallete
+- adjust styling
+
+- django s3 upload
+"""
+
+# Sample Inputs and output
+"""
+
+"""
+
+# Goal
+"""
+"""
+
+# Covering Our Bases - constraint
+"""
+"""
+
+# Create plan
+"""
+
+"""
+
+# Create code talk it out loud
