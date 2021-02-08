77a0231b0908304bffd22253305db431eabd4d8a
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-02-08 20:42:30 +0700

    Make settings works

--- app/settings/defaults.py
+++ app/settings/defaults.py
@@ -23,9 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '2)sq!49v@d7tfe&6$_$p46%nz$%sd-vt-dn72!+synwlivy^3-'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['127.0.0.1',
                  'localhost',
                  '0.0.0.0',
