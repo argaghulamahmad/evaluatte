4a6b27a6f2df93da8616669582cb6239a4acb982
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-02-27 14:19:13 +0700

    Disable pagination at the backend

--- app/settings/defaults.py
+++ app/settings/defaults.py
@@ -176,8 +176,8 @@ REST_FRAMEWORK = {
     #     'rest_framework.renderers.JSONRenderer',
     # ),
 
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 10
 }
 
 # Swagger
