3e5f05f62d369fdb6bf278961efdd715f34a35b0
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-02-27 14:46:50 +0700

    Remove unused router.py, serializers.py, and viewsets.py

--- api/router.py
+++ api/router.py
@@ -3,8 +3,6 @@ from rest_framework import routers
 from api import viewsets
 
 router = routers.DefaultRouter()
-router.register(r'users', viewsets.UserViewSet)
-router.register(r'groups', viewsets.GroupViewSet)
-router.register(r'consultants', viewsets.ConsultantViewSet)
+router.register(r'consultants', viewsets.ConsultantViewSet, basename='Consultants')
 router.register(r'companies', viewsets.CompanyViewSet, basename='Company')
 router.register(r'consultant-schedule', viewsets.ConsultantScheduleViewSet, basename='ConsultantSchedule')
--- api/viewsets.py
+++ api/viewsets.py
@@ -1,28 +1,9 @@
-from django.contrib.auth.models import User, Group
 from django.db.models import Prefetch
 from rest_framework import viewsets
 
-from api.serializers import UserSerializer, GroupSerializer, ConsultantSerializer, CompanySerializer, \
+from api.serializers import ConsultantSerializer, CompanySerializer, \
     ConsultantScheduleWithConsultantSerializer
 from core.models import Consultant, Company, ConsultantSchedule
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    # permission_classes = [permissions.IsAuthenticated, HasAPIKey]
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    # permission_classes = [permissions.IsAuthenticated, HasAPIKey]
 
 
 class ConsultantViewSet(viewsets.ModelViewSet):
