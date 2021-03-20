f3ae624b62c25b51d1781f5d6e54160ccef8bd08
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-03-20 08:53:47 +0700

    Create new order log

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from core.models import OrderLog
+
 
 @api_view(['POST'])
 def order(request):
@@ -30,6 +32,20 @@ def order(request):
     consultant_price = request.data.consultantPrice
     consultant_schedule_id = request.data.consultantScheduleId
     consultant_type = request.data.consultantType
+
+    new_order_log = OrderLog(
+        client_email=client_email,
+        client_name=client_name,
+        client_phone_number=client_phone_number,
+        client_problem=client_problem,
+        client_resume_url=client_resume_url,
+        consultant_id=consultant_id,
+        consultant_name=consultant_name,
+        consultant_price=consultant_price,
+        consultant_schedule_id=consultant_schedule_id,
+        consultant_type=consultant_type,
+    )
+    new_order_log.save()
 
     # todo create new row object and save using django - client
     # todo create new row object and save using django - meet
