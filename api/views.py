c158809ba3d700152ba112c55c904e9a5a422d78
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-03-20 09:47:26 +0700

    Fetch the data and then create new order log

--- api/views.py
+++ api/views.py
@@ -9,29 +9,29 @@ def order(request):
     """
     Example of the post data body:
     {
-        clientEmail: "test@gmail.com"
-        clientName: "test"
-        clientPhoneNumber: "0888888"
-        clientProblem: "asasas"
-        clientResumeUrl: "https://evaluatte-system.s3.amazonaws.com/CV/776-1613658265272.jpg.jpeg"
+        clientEmail: "arga@gmail.com"
+        clientName: "arga"
+        clientPhoneNumber: "087708770877"
+        clientProblem: "087708770877"
+        clientResumeUrl: "https://evaluatte-system.s3.amazonaws.com/CV/748-1613658265272.jpg.jpeg"
         consultantId: 1
         consultantName: "Teguh Priyantono"
         consultantPrice: "75000.00"
-        consultantScheduleId: 2
+        consultantScheduleId: 1
         consultantType: "CV"
     }
     """
 
-    client_email = request.data.clientEmail
-    client_name = request.data.clientName
-    client_phone_number = request.data.clientPhoneNumber
-    client_problem = request.data.clientProblem
-    client_resume_url = request.data.clientResumeUrl
-    consultant_id = request.data.consultantId
-    consultant_name = request.data.consultantName
-    consultant_price = request.data.consultantPrice
-    consultant_schedule_id = request.data.consultantScheduleId
-    consultant_type = request.data.consultantType
+    client_email = request.data['clientEmail']
+    client_name = request.data['clientName']
+    client_phone_number = request.data['clientPhoneNumber']
+    client_problem = request.data['clientProblem']
+    client_resume_url = request.data['clientResumeUrl']
+    consultant_id = request.data['consultantId']
+    consultant_name = request.data['consultantName']
+    consultant_price = request.data['consultantPrice']
+    consultant_schedule_id = request.data['consultantScheduleId']
+    consultant_type = request.data['consultantType']
 
     new_order_log = OrderLog(
         client_email=client_email,
