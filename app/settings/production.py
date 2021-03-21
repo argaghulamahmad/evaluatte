22ada1c8f2a4dae5de7eab5a1de4b7d93b2fd3e7
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-03-21 19:13:43 +0700

    Logging

--- app/settings/production.py
+++ app/settings/production.py
@@ -19,3 +19,61 @@ def show_toolbar(request):
 DEBUG_TOOLBAR_CONFIG = {
     "SHOW_TOOLBAR_CALLBACK": show_toolbar,
 }
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} [{asctime}] {message} {pathname} {lineno} {funcName}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'evaluatte-file-handler': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': '/home/argaghulamahmad/logs/evaluatte.log',
+            'formatter': 'verbose'
+        },
+        'evaluatte-file-handler-debug': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/home/argaghulamahmad/logs/evaluatte.debug.log',
+            'formatter': 'verbose'
+        },
+        'sql-debug-handler': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/home/argaghulamahmad/logs/evaluatte.sql-debug.log'
+        },
+        'django-error-handler': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            'filename': '/home/argaghulamahmad/logs/evaluatte.django.log'
+        },
+    },
+    'loggers': {
+        'evaluatte': {
+            'handlers': ['evaluatte-file-handler'],
+            'level': 'INFO',
+            'propagate': True
+        },
+        'evaluatte.debug': {
+            'handlers': ['evaluatte-file-handler-debug'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'django.db.backends': {
+            'handlers': ['sql-debug-handler'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'django': {
+            'handlers': ['django-error-handler'],
+            'level': 'ERROR',
+            'propagate': False
+        },
+    },
+}
--- reload.sh
+++ reload.sh
@@ -1,2 +1,2 @@
 kill -9 $(ps aux | grep 'evaluatte' | awk '{print $2}')
-gunicorn app.wsgi -b 0.0.0.0:8080 --reload --timeout 900 --log-level debug --log-file ~/logs/evaluatte.log --daemon
+gunicorn app.wsgi -b 0.0.0.0:8080 --reload --timeout 900 --log-level debug --log-file ~/logs/evaluatte.gunicorn.log --daemon
