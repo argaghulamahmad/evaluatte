f3a6f510a87dcbbbaec5afa64eda4784e53d4bfc
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-03-21 20:35:44 +0700

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,2 +1,2 @@
-kill -9 $(ps aux | grep 'evaluatte' | awk '{print $2}')
+kill -9 $(ps aux | grep 'evaluatte.gunicorn' | awk '{print $2}')
 gunicorn app.wsgi -b 0.0.0.0:8080 --reload --timeout 900 --log-level debug --log-file ~/logs/evaluatte.gunicorn.log --daemon
