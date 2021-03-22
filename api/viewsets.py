8b455a5d1bec2a95a7f0fab9af45de2876d7287d
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-03-22 19:09:05 +0700

    Set query for ConsultantViewSet

--- api/viewsets.py
+++ api/viewsets.py
@@ -1,10 +1,10 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, Q
 from rest_framework import mixins, viewsets
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from api.serializers import ConsultantSerializer, CompanySerializer, ConsultantScheduleSerializer, TestimonialSerializer
-from core.models import Consultant, Company, ConsultantSchedule, Meet, Client
+from core.models import Consultant, Company, ConsultantSchedule, Meet
 
 
 class FetchViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericViewSet):
@@ -14,6 +14,18 @@ class FetchViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericView
 class ConsultantViewSet(FetchViewSet):
     queryset = Consultant.objects.filter(is_active=True)
     serializer_class = ConsultantSerializer
+
+    def get_queryset(self):
+        query = """
+            select cc.full_name, ccs.id, ccs.is_booked
+             from core_consultant cc
+                      left join core_consultant_schedule ccs on cc.id = ccs.consultant_id
+             where ccs.is_booked is not True
+        """
+
+        consultants = (Consultant.objects.raw(query))
+
+        return consultants
 
 
 class CompanyViewSet(FetchViewSet):
