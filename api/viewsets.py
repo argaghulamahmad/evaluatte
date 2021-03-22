0c567ad62f77f30fdcbf8d2925e56abfb1cc464c
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-03-22 19:58:55 +0700

    Only return the not booked yet consultants schedule

--- api/viewsets.py
+++ api/viewsets.py
@@ -1,4 +1,3 @@
-from django.db import connection
 from django.db.models import Prefetch
 from rest_framework import mixins, viewsets
 from rest_framework.response import Response
@@ -6,15 +5,6 @@ from rest_framework.viewsets import GenericViewSet
 
 from api.serializers import ConsultantSerializer, CompanySerializer, ConsultantScheduleSerializer, TestimonialSerializer
 from core.models import Consultant, Company, ConsultantSchedule, Meet
-
-
-def dict_fetch_all(cursor):
-    """Return all rows from a cursor as a dict"""
-    columns = [col[0] for col in cursor.description]
-    return [
-        dict(zip(columns, row))
-        for row in cursor.fetchall()
-    ]
 
 
 class FetchViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericViewSet):
@@ -25,15 +15,9 @@ class ConsultantViewSet(FetchViewSet):
     serializer_class = ConsultantSerializer
 
     def get_queryset(self):
-        query = """
-            select *
-             from core_consultant cc
-                      left join core_consultant_schedule ccs on cc.id = ccs.consultant_id
-             where ccs.is_booked is not True
-        """
-
-        consultants = (Consultant.objects.raw(query))
-
+        consultants = Consultant.objects.filter(is_active=True).prefetch_related(
+            Prefetch('consultant_schedules', queryset=ConsultantSchedule.objects.filter(is_booked=False)),
+        )
         return consultants
 
 
