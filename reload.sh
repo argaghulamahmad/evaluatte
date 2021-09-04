af5a239a47e204caa45fd3a811039c855f39e035
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-09-04 14:31:02 +0700

    remove unused files

--- reload.sh
+++
@@ -1,2 +0,0 @@
-kill -9 $(ps aux | grep 'evaluatte.gunicorn' | awk '{print $2}')
-gunicorn app.wsgi -b 0.0.0.0:8080 --reload --timeout 900 --log-level debug --log-file ~/logs/evaluatte.gunicorn.log --daemon
